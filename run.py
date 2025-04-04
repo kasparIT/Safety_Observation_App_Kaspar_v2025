2ab511ee19efadb16147b605a14f89ff92d03abe
kasparIT <azureadmin@kasparcompanies.com>
2025-04-04 16:11:38 -0500

    update css

--- run.py
+++ run.py
@@ -6,4 +6,5 @@ app = create_app()
 
 if __name__ == "__main__":
     print("Starting Flask server...")  # Debugging output
-    app.run(debug=True)
+    #app.run(debug=True) # For development purposes only
+    app.run(host="0.0.0.0", port=5000, debug=True) # For production use, set debug=False
