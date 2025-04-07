0273c39af3f2e6363a9f3648646137947dfd632e
Krushan Panchal <46457493+termigator26@users.noreply.github.com>
2025-04-07 11:03:37 -0500

    Production change

--- run.py
+++ run.py
@@ -7,4 +7,4 @@ app = create_app()
 if __name__ == "__main__":
     print("Starting Flask server...")  # Debugging output
     #app.run(debug=True) # For development purposes only
-    app.run(host="0.0.0.0", port=5000, debug=True) # For production use, set debug=False
+    app.run(host="0.0.0.0", port=5000, debug=False) # For production use, set debug=False
