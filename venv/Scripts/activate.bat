203b98f4e8a67d989ff0ef46f64b1ae5ddb97c44
Krushan Panchal <krushandpanchal@gmail.com>
2025-04-16 10:20:53 -0500

    venv

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\Krushan\OneDrive\Documents\Krushan Github Projects\Safety_Observation_App_Kaspar_v2025\venv"
+set "VIRTUAL_ENV=D:\Krushan Files\GitHub Projects\Safety_Observation_App_Kaspar_v2025\venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
